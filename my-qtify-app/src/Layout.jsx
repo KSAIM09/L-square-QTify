158bf278374f339a6548f3b9f31644c9c359ac11
Saim Khan <93315183+KSAIM09@users.noreply.github.com>
2023-11-11 11:11:12 +0000

    tabsFilter-qtify-takehome

--- my-qtify-app/src/Layout.jsx
+++ my-qtify-app/src/Layout.jsx
@@ -4,6 +4,7 @@ import HeroSection from "./Component/HeroSection/HeroSection";
 import Card from "./Component/Card/Card";
 import TopAlbum from "./Component/topAlbum/TopAlbum";
 import NewAlbum from "./Component/newAlbum/NewAlbum";
+import Songs from "./Component/SongNFilter/Songs"
 import "./Layout.css";
 
 
@@ -15,6 +16,7 @@ const Layout = () => {
             <Card />
             <TopAlbum />
             <NewAlbum />
+            <Songs />
         </div>
     );
 };
