ecbfba00828ff6f40a963920893763cd2a4f2afb
Saim Khan <93315183+KSAIM09@users.noreply.github.com>
2023-11-11 11:47:48 +0000

    Installing package

--- my-qtify-app/src/Component/Accordion/ExploreAccordion.jsx
+++ my-qtify-app/src/Component/Accordion/ExploreAccordion.jsx
@@ -7,44 +7,44 @@ import "./Accordion.css";
 
 const ExploreAccordion = ({ data }) => {
     return (
-            <div className="exploreaccordion">
-                <Accordion style={{ border: "1px solid #FFFFFF", borderRadius: "10px" }}>
-                <AccordionSummary
-                    style={{
-                    background: "#121212",
-                    borderRadius: "10px",
-                    width: "1135px",
-                    height: "78px",
-                }}
-                expandIcon={
-                    <ExpandMoreIcon
-                        style={{ color: "#34C94B", width: "55px", height: "40px", scale: "2" }}
-                    />
-                }
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-                >
-                    <Typography style={{ fontSize: "20px", color: "#FFFFFF" }}>
-                        {data.question}
-                    </Typography>
-                </AccordionSummary>
-                <AccordionDetails
-                    style={{
-                    border: "1px solid #FFFFFF",
-                    background: "#FFFFFF",
-                    borderRadius: "0px 0px 10px 10px",
-                    width: "1135px",
-                    height: "70px",
-                    overflow: "scroll",
-                }}
-            >
-                <Typography style={{ fontSize: "25px", color: "#121212" }}>
-                    {data.answer}
-                </Typography>
-            </AccordionDetails>
+      <div className="exploreaccordion">
+        <Accordion style={{ border: "1px solid #FFFFFF", borderRadius: "10px" }}>
+          <AccordionSummary
+            style={{
+              background: "#121212",
+              borderRadius: "10px",
+              width: "1135px",
+              height: "78px",
+            }}
+            expandIcon={
+              <ExpandMoreIcon
+                style={{ color: "#34C94B", width: "55px", height: "40px", scale: "2" }}
+              />
+            }
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography style={{ fontSize: "20px", color: "#FFFFFF" }}>
+              {data.question}
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails
+            style={{
+              border: "1px solid #FFFFFF",
+              background: "#FFFFFF",
+              borderRadius: "0px 0px 10px 10px",
+              width: "1135px",
+              height: "70px",
+              overflow: "scroll",
+            }}
+          >
+            <Typography style={{ fontSize: "25px", color: "#121212" }}>
+              {data.answer}
+            </Typography>
+          </AccordionDetails>
         </Accordion>
-        </div>
+      </div>
     );
-};
-
-export default ExploreAccordion;
+  };
+  
+  export default ExploreAccordion;
